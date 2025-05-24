f47fca9d528f332d9be5e5f3e1dd6f399e63630d
Barton Hammond <bartonhammond@gmail.com>
2025-05-24 14:43:58 -0500

    initial

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+	<div id="app">
+		<YourComponent />
+	</div>
+</template>
+
+<script setup>
+	import YourComponent from "./components/YourComponent.vue";
+</script>
+
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,11 @@
+<script setup>
+	import { UserIcon, QRLoad, AccountHome } from "@gun-vue/components";
+</script>
+
+<template>
+	<div>
+		<UserIcon />
+		<QRLoad />
+		<AccountHome />
+	</div>
+</template>
